57e7b309cac0a4e27e13abf749c216a09b917aaa
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-04-15 14:36:55 +0300

    feat: Create the users controller

--- src/Migrations/20210415120649_set_username_as_unique.Designer.cs
+++ src/Migrations/20210415120649_set_username_as_unique.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using src;
 
 namespace src.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210415120649_set_username_as_unique")]
+    partial class set_username_as_unique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace src
 {
@@ -26,5 +27,30 @@ namespace src
     public bool isDisabled { get; set; }
 
     public bool isDeleted { get; set; }
+  }
+
+  public class UserDto {
+    public int Id {get; set;}
+    public Person person {get; set;}
+    public string username {get; set;}
+    public bool isAdmin {get; set;}
+    public bool isActive {get; set;}
+    public bool isDisabled {get; set;}
+
+    public bool isDeleted {get; set;}
+
+    public UserDto() {
+
+    }
+
+    public UserDto(User user) {
+      Id = user.Id;
+      username = user.username;
+      person = user.person;
+      isActive = user.isActive;
+      isAdmin = user.isAdmin;
+      isDeleted = user.isDeleted;
+      isDisabled = user.isDisabled;
+    }
   }
 }
