4b8456dde91a03d10a746317642e60fa22791da8
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-04-17 17:28:42 +0300

    feat(backend): Add the polls controller

---
+++ src/Controllers/PollsController.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace src.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class PollsController : ControllerBase
+  {
+    private readonly ILogger<PollsController> _logger;
+    private readonly ApplicationDbContext _context;
+
+
+    public PollsController(
+      ILogger<PollsController> logger,
+      ApplicationDbContext context
+    )
+    {
+      _logger = logger;
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Question>>> list()
+    {
+
+      var polls = await this._context.Questions
+        .Where(q => q.additionalType == Question.POLL)
+        .Include("answers")
+        .ToArrayAsync();
+
+      return polls;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Question>> show(int id)
+    {
+      Question poll = await this._context.Questions
+        .Where(q => q.Id == id)
+        .Include("answers")
+        .FirstOrDefaultAsync();
+
+      if (poll is null)
+      {
+        return NotFound();
+      }
+
+      return poll;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Question>> create(Question question)
+    {
+
+      question.additionalType = Question.POLL;
+      if (question.datePublished.Equals(new DateTime()))
+      {
+        question.datePublished = DateTime.UtcNow;
+      }
+
+      string message = null;
+      if (question.optionSet is null)
+      {
+        message = "Option set can not be empty";
+      }
+      else if (question.dateCreated > question.endDate)
+      {
+        message = "End date can not be earlier than start date";
+      }
+      else if (question.datePublished > question.endDate)
+      {
+        message = "Published date can not be earlier than start date";
+      }
+
+      if (!(message is null))
+      {
+        object error = new
+        {
+          message = message
+        };
+        return BadRequest(error);
+      }
+
+      this._context.Questions.Add(question);
+      await this._context.SaveChangesAsync();
+      return question;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Question>> update(int id, PollUpdateDto q)
+    {
+      if (id != q.Id)
+      {
+        return BadRequest();
+      }
+
+      Question localQ = await this._context.Questions.Where(q => q.Id == id).FirstOrDefaultAsync();
+      if (localQ is null)
+      {
+        return NotFound();
+      }
+
+      var question = new Question()
+      {
+        Id = localQ.Id,
+        title = q.title is null ? localQ.title : q.title,
+        startDate = q.startDate.Equals(new DateTime()) ? localQ.startDate : q.datePublished,
+        endDate = q.endDate.Equals(new DateTime()) ? localQ.endDate : q.datePublished,
+        datePublished = q.datePublished.Equals(new DateTime()) ? localQ.datePublished : q.datePublished,
+        description = q.description is null ? localQ.description : q.description,
+        additionalType = localQ.additionalType,
+        alternateName = localQ.alternateName,
+        optionSet = localQ.optionSet,
+        dateModified = DateTime.UtcNow,
+        dateCreated = localQ.dateCreated,
+        answers = localQ.answers is null ? new List<VoteAction>() : localQ.answers
+      };
+
+      this._context.Entry(localQ).State = EntityState.Detached;
+      this._context.Entry(question).State = EntityState.Modified;
+
+      await this._context.SaveChangesAsync();
+      Question updated = await this._context.Questions
+        .Where(q => q.Id == id)
+        .Include("answers")
+        .FirstOrDefaultAsync();
+
+      return updated;
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Question>> delete(int id)
+    {
+
+      // Find the user in the database
+      var question = await _context.Questions.FindAsync(id);
+      if (question == null)
+      {
+        return NotFound();
+      }
+
+      this._context.Entry(question).State = EntityState.Deleted;
+      await this._context.SaveChangesAsync();
+      return question;
+    }
+  }
+
+  public class PollUpdateDto
+  {
+    public int Id { get; set; }
+    public string title { get; set; }
+
+    public string description { get; set; }
+    public DateTime startDate { get; set; }
+
+    public DateTime endDate { get; set; }
+
+    public DateTime datePublished { get; set; }
+  }
+}
---
+++ src/Migrations/20210417162957_update_questions_and_votes.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace src.Migrations
+{
+    public partial class update_questions_and_votes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VoteActions_Users_agentId",
+                table: "VoteActions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VoteActions_agentId",
+                table: "VoteActions");
+
+            migrationBuilder.DropColumn(
+                name: "agentId",
+                table: "VoteActions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "userId",
+                table: "VoteActions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VoteActions_userId",
+                table: "VoteActions",
+                column: "userId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VoteActions_Users_userId",
+                table: "VoteActions",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VoteActions_Users_userId",
+                table: "VoteActions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_VoteActions_userId",
+                table: "VoteActions");
+
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "VoteActions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "agentId",
+                table: "VoteActions",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VoteActions_agentId",
+                table: "VoteActions",
+                column: "agentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VoteActions_Users_agentId",
+                table: "VoteActions",
+                column: "agentId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
