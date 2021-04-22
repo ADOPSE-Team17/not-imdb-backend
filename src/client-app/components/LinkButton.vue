1383ffd1dfc8a145b3e886f1d815c7eec1fd6894
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-04-22 23:17:51 +0300

    fix(frontend): Fix the frontend links

---
+++ src/client-app/components/LinkButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-col justify="center" align="center">
+    <router-link :to="target" class="black white--text">
+      <v-btn> {{ text }} </v-btn>
+    </router-link>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "LinkButton",
+  props: {
+    text: {
+      type: String,
+    },
+    target: {
+      text: {
+        type: String,
+      },
+    },
+  },
+};
+</script>
--- src/client-app/layouts/default.vue
+++ src/client-app/layouts/default.vue
@@ -8,22 +8,17 @@
         class="amber lighten-2"
       >
       <div class="ml-16">
-        <Title/>
+        <router-link to="/">
+          <Title/>
+        </router-link>
       </div>
-      
-      
+      <div class="ml-auto d-flex">
         <Avatar
           v-show="loggedIn"
         />
-      
-      
-        <LoginButton
-          v-show="!loggedIn"
-        />
-        
-        <RegisterButton
-          v-show="!loggedIn"
-        />
+        <LinkButton v-show="!loggedIn" target="/Login" text="Login" ></LinkButton>
+        <LinkButton v-show="!loggedIn" target="/Register" text="Register" ></LinkButton>
+      </div>
     </v-app-bar>
         <!-- The nuxt element is where the whole app takes place -->
         <v-main>
@@ -42,8 +37,8 @@
 <script>
 import Title from '@/components/Title'
 import Avatar from '@/components/Avatar'
-import LoginButton from '@/components/LoginButton'
-import RegisterButton from '@/components/RegisterButton'
+import LinkButton from '@/components/LinkButton'
+
 export default {
   components: {
     Title,
