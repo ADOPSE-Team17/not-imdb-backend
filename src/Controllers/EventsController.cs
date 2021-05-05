114ad6b246fe5db665abc6db1cb7a5bf190a02e0
danaiii <64918738+danaiii@users.noreply.github.com>
2021-05-05 21:30:48 +0300

    Create EventsController.cs

---
+++ src/Controllers/EventsController.cs
@@ -0,0 +1,123 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace src.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class EventsController : ControllerBase
+  {
+
+    private readonly ILogger<EventsController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public EventsController(
+      ILogger<EventsController> logger,
+      ApplicationDbContext context
+    )
+    {
+      _logger = logger;
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Event>>> Get()
+    {
+        var allevents = await this._context.Events
+            .Include("movies")
+            .ToArrayAsync();
+        return allevents;
+    }
+
+    [HttpGet("free")]
+    public async Task<ActionResult<IEnumerable<Event>>> GetFreeEvents()
+    {
+        var freeEvents = await this._context.Events
+            .Include("movies")
+            .Where(m => m.isAccessibleForFree == true)
+            .ToArrayAsync();
+        return freeEvents;
+    }
+
+    [HttpGet("nearevents/{location}")]
+    public async Task<ActionResult<IEnumerable<Event>>> GetNearEvents(string location)
+    {
+        var nearEvents = await this._context.Events
+            .Include("movies")
+            .Where(m => m.location == location)
+            .ToArrayAsync();
+        return nearEvents;
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Event>> Getevent(int id)
+    {
+      var anEvent = await this._context.Events
+        .Include("movies")
+        .Where(m => m.Id == id)
+        .FirstOrDefaultAsync();
+
+      if (anEvent is null)
+      {
+        return NotFound();
+      }
+
+      return anEvent;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Event>> CreateEvent(Event anEvent)
+    {
+        this._context.Events.Add(anEvent);
+        await _context.SaveChangesAsync();
+        return anEvent;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Event>> UpdateEvent(int id, Event anEvent) 
+    {
+        if (id != anEvent.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(anEvent).State = EntityState.Modified;
+        var localevent = await this._context.Events.Where(m => m.Id == id).FirstOrDefaultAsync();
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (anEvent is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return anEvent;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Event>> DeleteEvent(int id)
+    {
+        var anEvent = await _context.Events.FindAsync(id);
+        if (anEvent == null)
+        {
+            return NotFound();
+        }
+
+        _context.Events.Remove(anEvent);
+        await _context.SaveChangesAsync();
+
+        return anEvent;
+    }
+  }
+}
