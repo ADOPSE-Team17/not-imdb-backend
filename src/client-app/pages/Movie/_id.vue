977c096133d4203653ae9e17b58f287608e124b9
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-05-14 17:28:09 +0300

    fix: Add the movie full view subscription

--- src/client-app/pages/Movie/_id.vue
+++ src/client-app/pages/Movie/_id.vue
@@ -1,53 +1,48 @@
 <template>
   <div class="pa-16">
-      <small>ID : {{this.$route.params.id}}</small>
-      <Movie
-        :movieTitle="this.movie.headline"
-        :about="this.movie.about"
-        :abstract="this.movie.abstractText"
-        :creator="this.movie.creator"
-        :producer="this.movie.producer"
-        :musicBy="this.movie.musicBy"
-        :familyFriendly="this.movie.isFamilyFriendly"
-        :trailer="this.movie.trailerUrl"
-      />
+    <small>ID : {{ this.$route.params.id }}</small>
+    <v-sheet>
+      <v-skeleton-loader
+        v-if="!movie"
+        class="mx-auto"
+        max-width="400"
+        type="card"
+      ></v-skeleton-loader>
+    </v-sheet>
+    <Movie
+      v-if="movie"
+      :movieTitle="this.movie.headline"
+      :about="this.movie.about"
+      :abstract="this.movie.abstractText"
+      :creator="this.movie.creator"
+      :producer="this.movie.producer"
+      :musicBy="this.movie.musicBy"
+      :familyFriendly="this.movie.isFamilyFriendly"
+      :trailer="this.movie.trailerUrl"
+    />
   </div>
 </template>
 
 <script>
-import Movie from '@/components/Movie.vue'
+import Movie from "@/components/Movie.vue";
 
 export default {
-  components:{
-      Movie
+  components: {
+    Movie,
   },
-  data(){
-      return{
-          movie : this.$store.getters['movies/getFetchedMovieById']
+  data() {
+    return { 
+      movie: undefined
+    };
+  },
+  methods: {},
+  created() {
+    this.$store.subscribe((mutation) => {
+      if (mutation.type === "movies/FETCH_MOVIE_BY_ID") {
+        this.movie = this.$store.getters["movies/getFetchedMovieById"];
       }
+    });
+    this.$store.dispatch("movies/fetchMovieById", this.$route.params.id);
   },
-  methods:{
-  
-  },
-  created(){
-    this.$store.dispatch('movies/fetchMovieById',this.$route.params.id)
-  },
-  updated(){
-    this.$store.dispatch('movies/fetchMovieById',this.$route.params.id).then((data) => {
-  console.log(data);
-}).catch((err) => {
-console.error(err);
-});
-  },
-  watch:{
-    $route (to, from){
-        console.log(to,from)
-       this.$store.dispatch('movies/fetchMovieById',this.$route.params.id)
-    }
-    }
-
-}
-
+};
 </script>
-
-<style>
