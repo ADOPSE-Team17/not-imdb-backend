8a06da4192f4c6b778bacba12dec3c3ca7dbb048
Dimitris Pliakos <dimitrispl96@gmail.com>
2021-05-15 22:56:43 +0300

    feat: Use the authorization token at frontend

--- src/client-app/pages/Login/index.vue
+++ src/client-app/pages/Login/index.vue
@@ -1,67 +1,63 @@
 <template>
   <div class="container">
-    <LoginCard :loginForm="loginUser"/>
+    <LoginCard :loginForm="loginUser" />
   </div>
 </template>
 
 <script>
-import LoginCard from '@/components/LoginCard.vue'
+import LoginCard from "@/components/LoginCard.vue";
 
-export default{
-    components: {
-        LoginCard    
-    },
-    methods:{
-      loginUser(info){
-        try {
-          this.$store.dispatch('auth/loginUser',info)
-        } catch (error) {
-          console.log(error)
-        }
-        
+export default {
+  components: {
+    LoginCard,
+  },
+  methods: {
+    loginUser(info) {
+      try {
+        this.$store.dispatch("auth/loginUser", info);
+      } catch (error) {
+        console.log(error);
       }
     },
-  
-    
-}
-
+  }
+};
 </script>
 
 <style>
- .custom-loader {
-    animation: loader 1s infinite;
-    display: flex;
+.custom-loader {
+  animation: loader 1s infinite;
+  display: flex;
+}
+@-moz-keyframes loader {
+  from {
+    transform: rotate(0);
   }
-  @-moz-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+  to {
+    transform: rotate(360deg);
   }
-  @-webkit-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+}
+@-webkit-keyframes loader {
+  from {
+    transform: rotate(0);
   }
-  @-o-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+  to {
+    transform: rotate(360deg);
   }
-  @keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+}
+@-o-keyframes loader {
+  from {
+    transform: rotate(0);
   }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@keyframes loader {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
 </style>
--- src/client-app/store/auth.js
+++ src/client-app/store/auth.js
@@ -1,31 +1,55 @@
-import axios from 'axios'
+import axios from 'axios';
 
-
-export default{
+export default {
     namespaced: true,
-    state(){
-        return{
+    state() {
+        return {
             registerInfo: {},
-            loginInfo:{
-                identifier:'',
-                password:''
+            loginInfo: {
+                identifier: '',
+                password: ''
             },
-            loggedIn:false,
-            isAdmin:false
+            loggedIn: false,
+            isAdmin: false,
+            loading: true,
+            hasError: false
         }
     },
     mutations: {
         REGISTER_USER(state, info) {
             state.registerInfo = info
             state.loggedIn = true
+            state.loading = false;
         },
-        LOGIN_USER(state,info,admin){
-            state.loginInfo = info 
+        LOGIN_USER(state, info) {
+            state.loginInfo = info.user
             state.loggedIn = true
-            // console.log(state.loggedIn)
-            if (admin){
+            state.loading = false;
+
+            if (window && info.token) {
+                window.localStorage.setItem('token', info.token);
+            }
+
+            if (info.isAdmin) {
                 state.isAdmin = true
             }
+            console.log('it should log');
+            return this.info;
+        },
+        LOGOUT_USER(state) {
+            state.loading = false;
+            state.isAdmin = false;
+            state.hasError = false;
+            state.loggedIn = false;
+            state.loginInfo = undefined;
+            state.registerInfo = undefined;
+            if (window) {
+                window.localStorage.removeItem('token');
+                window.location = "/";
+            }
+        },
+        LOGIN_FAIL(state, info) {
+            state.loading =false;
         }
     },
     actions: {
@@ -37,27 +61,53 @@ export default{
                 "passwordRepeat": info.passwordRepeat
             }
             try {
-                const res = await axios.post('http://localhost:5000/Auth/Register',data)
-                // console.log(res)
-                commit('REGISTER_USER',info )
+                const res = await axios.post('http://localhost:5000/Auth/Register', data)
+                commit('REGISTER_USER', info)
             } catch (error) {
-                console.log(error)
+                console.error(error)
             }
         },
 
-        async loginUser({commit},info){
+        async loginUser({ commit }, info) {
             const data = {
-                "identifier" : info.identifier,
-                "password" : info.password
+                "identifier": info.identifier,
+                "password": info.password
             }
 
             try {
-                const res = await axios.post('http://localhost:5000/Auth/Login',data)
-                // console.log(res.data)
-                commit('LOGIN_USER',info,res.data.isAdmin)
+                const res = await axios.post('http://localhost:5000/Auth/Login', data);
+                if (window) {
+                    const token = window.localStorage.setItem('token', data.token);
+                    if (token) {
+                        axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+                    }
+                }
+                commit('LOGIN_USER', res.data);
             } catch (error) {
-                console.log(error)
+                console.error(error);
+                commit('LOGIN_FAIL');
             }
+        },
+        async verifyUser({ commit }) {
+            if (window) {
+                const token = window.localStorage.getItem('token');
+                if (token) {
+                    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                }
+            }
+
+            try {
+                const { data: user } = await axios.get('http://localhost:5000/Auth/verify');
+                commit('LOGIN_USER', {
+                    user
+                });
+            } catch (err) {
+                console.error(err);
+                commit('LOGIN_FAIL');
+            }
+        },
+        logoutUser({commit}) {
+            commit('LOGOUT_USER');
         }
     },
 
@@ -73,6 +123,9 @@ export default{
        },
        isAdmin:(state) => {
            return state.isAdmin
+       },
+       state: (state) => {
+           return state;
        }
     }
 }
